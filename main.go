04767ab2bc8a1fccf754061b447e338bfe51ddcf
Rishabh Singh <rishabhsingh2305@gmail.com>
2025-09-18 00:00:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,26 +3,57 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"log" // <-- Import the log package
 	"os"
 	"strings"
 
 	"github.com/fatih/color"
+	"github.com/hpcloud/tail"
 )
 
-// all time best time : 0.30s user 0.02s system 103% cpu 0.304 total
-// cur best time      : 0.34s user 0.01s system 103% cpu 0.341 total
-
-var blueBox = color.New(color.BgBlue, color.FgWhite).SprintFunc()
-var redBox = color.New(color.BgRed, color.FgWhite).SprintFunc()
-var greenBox = color.New(color.BgGreen, color.FgWhite).SprintFunc()
-var yellowBox = color.New(color.BgYellow, color.FgWhite).SprintFunc()
+var (
+	blueBox   = color.New(color.BgBlue, color.FgWhite).SprintFunc()
+	redBox    = color.New(color.BgRed, color.FgWhite).SprintFunc()
+	greenBox  = color.New(color.BgGreen, color.FgWhite).SprintFunc()
+	yellowBox = color.New(color.BgYellow, color.FgWhite).SprintFunc()
+)
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	if len(os.Args) > 1 {
+		filename := os.Args[1]
+		processFile(filename)
+	} else {
+		processStdin()
+	}
+}
 
-	// Create a buffered writer for stdout
+func processFile(filename string) {
+	config := tail.Config{
+		Follow:   true,
+		ReOpen:   true,
+		Location: &tail.SeekInfo{Offset: 0, Whence: io.SeekEnd},
+		Logger: log.New(io.Discard, "", 0),
+	}
+	t, err := tail.TailFile(filename, config)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error: ", err)
+		return
+	}
+
 	writer := bufio.NewWriter(os.Stdout)
-	// CRITICAL: Ensure the buffer is flushed before the program exits.
+	defer writer.Flush()
+
+	for line := range t.Lines {
+		highlighted := highlightLog(line.Text)
+		fmt.Fprintln(writer, highlighted)
+		writer.Flush()
+	}
+}
+
+func processStdin() {
+	scanner := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
 
 	for scanner.Scan() {
@@ -32,32 +63,21 @@ func main() {
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		fmt.Fprintln(os.Stderr, "error reading standard input:", err)
 	}
 }
 
 func highlightLog(line string) string {
-	// Use an if/else if chain to short-circuit
 	if strings.Contains(line, "ERROR") {
 		return strings.ReplaceAll(line, "ERROR", redBox(" ERROR "))
-	}
-
-	if strings.Contains(line, "WARNING") { // Important: Check for "WARNING" before "WARN"
+	} else if strings.Contains(line, "WARNING") {
 		return strings.ReplaceAll(line, "WARNING", yellowBox(" WARNING "))
-	}
-
-	if strings.Contains(line, "WARN") {
+	} else if strings.Contains(line, "WARN") {
 		return strings.ReplaceAll(line, "WARN", yellowBox(" WARN "))
-	}
-
-	if strings.Contains(line, "INFO") {
+	} else if strings.Contains(line, "INFO") {
 		return strings.ReplaceAll(line, "INFO", greenBox(" INFO "))
-	}
-
-	if strings.Contains(line, "DEBUG") {
+	} else if strings.Contains(line, "DEBUG") {
 		return strings.ReplaceAll(line, "DEBUG", blueBox(" DEBUG "))
 	}
-
-	// If no keywords are found, return the original line
 	return line
 }
